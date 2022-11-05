5bf228fd0ab70c0dec6428ce5a58606001a43efd
Esteban Medero <estebanmedero@Estebans-MBP.attlocal.net>
2022-11-05 18:36:01 -0400

    get function implemented

--- maillist/app.js
+++ maillist/app.js
@@ -4,6 +4,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+
+    res.sendFile(__dirname + "/signup.html");
+});
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000.");
